df6416f9f008dfd21b6013c9da024799d2927e42
ksubnd <ksubnd@gmail.com>
2026-04-18 22:06:03 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,33 +2,34 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <iostream>
 template<typename T, int size>
 class TStack {
- private:
-   T arr[size];
-   int top;
- public:
-   TStack() : top(-1) {}
-   int getSize() const { return top+1; }
-   bool isEmpty() const { return top == -1; }
-   void push(const T& item) {
-     if (top < size - 1)
-       arr[++top] = item;
-     else
-       throw "Full stack";
-   }
-   T get() const {
-     if (top >= 0)
-       return arr[top];
-     else
-       throw "Empty stack";
-   }
-   void pop() {
-     if (top >= 0)
-       top--;
-     else
-       throw "Empty stack";
-   }
+  private:
+    T arr[size];
+    int top;
+  public:
+    TStack() : top(-1) {}
+    int getSize() const { return top+1; }
+    bool isEmpty() const { return top == -1; }
+    void push(const T& item) {
+      if (top < size - 1)
+        arr[++top] = item;
+      else
+        throw "Full stack";
+    }
+    T get() const {
+      if (top >= 0)
+        return arr[top];
+      else
+        throw "Empty stack";
+    }
+    void pop() {
+      if (top >= 0)
+        top--;
+      else
+        throw "Empty stack";
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
